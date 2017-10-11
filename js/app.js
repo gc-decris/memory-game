2d7d3aae823331f47de64d8f16b0e74cbd3ef50b
Giancarlo DeCristofaro <g.decristofaro@yahoo.com>
2017-10-11 01:23:13 -0400

    refactor: Rearrange JS code to improve readability.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
 var deckList = [];
 
-$('.card').each(function(){
+$('.card').each(function () {
     deckList.push($(this));
 });
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -28,7 +16,7 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 shuffle(deckList);
 
 for (x = 0; x < deckList.length; x++) {
@@ -36,26 +24,24 @@ for (x = 0; x < deckList.length; x++) {
     $('.deck').append(card);
 };
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- *    ++ SHUFFLE DECK WHEN YOU HIT THE RELOAD ICON  
- */
 // classes open and show = flipped card --- class match = matched card
 var selected = [];
 var moveCount = 0;
 
-function checkCards (array) {
+function unmatched(one, two) {
+    $(one).removeClass('open show');
+    $(two).removeClass('open show');
+};
+
+function matched(one, two) {
+    $(one).addClass('match');
+    $(two).addClass('match');
+};
+
+function checkCards(array) {
     var cardOne = array[0];
     var cardTwo = array[1];
-    if ($(cardOne).find('i').attr('class') === $(cardTwo).find('i').attr('class')){
+    if ($(cardOne).find('i').attr('class') === $(cardTwo).find('i').attr('class')) {
         matched(cardOne, cardTwo);
     } else {
         unmatched(cardOne, cardTwo);
@@ -63,22 +49,12 @@ function checkCards (array) {
     return selected = [];
 };
 
-function unmatched(one, two) {
-    $(one).removeClass('open show');
-    $(two).removeClass('open show');
-}
-
-function matched(one, two) {
-    $(one).addClass('match');
-    $(two).addClass('match');
-}
-
 function moveCounter() {
     $('.moves').empty();
     $('.moves').text(moveCount);
-}
+};
 
-function checkWin(){
+function checkWin() {
     var win = true
     for (var index = 0; index < deckList.length; index++) {
         var cards = deckList[index];
@@ -90,7 +66,20 @@ function checkWin(){
         $('.score').text(moveCount);
         $('.alert').toggleClass('hidden');
     }
-}
+};
+
+$('.card').click(function () {
+    $(this).addClass('open show')
+    selected.push($(this));
+    if (selected.length == 2) {
+        setTimeout(function () {
+            checkCards(selected);
+            moveCount = moveCount + 1;
+            moveCounter();
+            checkWin();
+        }, 1000);
+    }
+});
 
 $('button').click(function(){
     $('.card').toggleClass('open show match');
@@ -101,19 +90,6 @@ $('button').click(function(){
         $('.deck').append(card);
     };
 });
-
-$('.card').click(function(){
-    $(this).addClass('open show')
-    selected.push($(this));
-    if (selected.length == 2){
-        setTimeout(function() {
-        checkCards(selected); 
-        moveCount = moveCount + 1;
-        moveCounter(); 
-        checkWin();
-        }, 1000);
-    }
-})
 
 $('.restart').click(function () {
     $('.card').removeClass('open show match');
