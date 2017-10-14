08b6f49bbaf458ab732c2ac647172b579cd2ee5c
Giancarlo DeCristofaro <g.decristofaro@yahoo.com>
2017-10-14 12:39:59 -0400

    chore: Removed comments from Udacity Project Rubric guide

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// ******STAR RATING FEATURE******
-// The game displays a star rating (from 1- 3) that reflects the player's performance. At the beginning of a game, it should display 3 stars. After some number of moves, it should change to a 2 star rating. After a few more moves, it should change to a 1 star rating.
-
-// The number of moves needed to change the rating is up to you, but it should happen at some point
-
-// ******TIMER FEATURE*******
-// When the player starts a game, a timer should also start.Once the player wins the game, the timer stops.
-
-// ******CHANGES REQUIRED ON THE CONGRATS ALERT******
-// When a user wins the game, a modal appears to congratulate the player and ask if they want to play again.It should also tell the user how much time it took to win the game, and what the star rating was.
 
 var deckList = [];
 
